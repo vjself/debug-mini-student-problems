9f6289aa8c95d7c4a411e3b24c89f36de54f8b12
Tyler Collier <tyler.collier+github@devmounta.in>
2018-10-22 23:53:00 -0700

    Add base component

---
+++ src/MyComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './MyComponent.css';
+
+export default class MyComponent extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {};
+  // }
+
+  render() {
+    return (
+      <div className="my-component-container">
+        <h2>My Component</h2>
+      </div>
+    );
+  }
+}
