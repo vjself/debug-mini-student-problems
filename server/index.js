dffb6ab9ea1e87307386f74e6b94c1be6a66cfd9
Tyler Collier <tyler.collier+github@devmounta.in>
2018-10-23 01:21:37 -0700

    Create problem 5

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-app.use(bodyParser.json());
 
 app.post('/api/data', (req, res) => {
   res.json({ message: `The value you sent was ${req.body.value}` });
