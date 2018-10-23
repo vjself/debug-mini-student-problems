b8311f2f774239881a692caea99858c314f3258a
Tyler Collier <tyler.collier+github@devmounta.in>
2018-10-22 23:33:28 -0700

    Add server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+const SERVER_PORT = process.env.SERVER_PORT || 4000;
+app.listen(SERVER_PORT, () => {
+  console.log('Server listening on port ' + SERVER_PORT);
+});
